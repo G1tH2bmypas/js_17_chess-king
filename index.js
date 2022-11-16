2b692a0f1a3831e30e219c1149a46c963a29388c
Tekyto <Tekyto2014@gmail.com>
2022-11-16 20:08:47 +0200

    js_17_chess-king+

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function king(x1, y1, x2, y2) {
-	// write your code here
+	return y1 === y2 && x2 === x1 + 1 || y1 === y2 && x2 === x1 - 1 || x1 === x2 && y2 === y1 - 1 || x1 === x2 && y2 === y1 + 1 || x2 === x1 - 1 && y2 === y1 - 1 || x2 === x1 - 1 && y2 === y1 + 1 || x2 === x1 + 1 && y2 === y1 + 1 || x2 === x1 + 1 && y2 === y1 - 1;
 }
 
 module.exports = king;
